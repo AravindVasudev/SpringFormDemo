c494f953c01137022e0af84acf92274d1cc3410a
Aravind Vasudevan <aravind_vasudevan@outlook.com>
2019-03-23 18:31:36 +0530

    Added missing imports

--- src/main/java/com/example/SpringFormDemo/DemoApplication.java
+++ src/main/java/com/example/SpringFormDemo/DemoApplication.java
@@ -2,6 +2,8 @@ package com.example.SpringFormDemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.*
 
 @Controller
 @SpringBootApplication
