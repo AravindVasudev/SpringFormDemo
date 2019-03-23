7ff1eab1bcaa59b8e8bb04b1ae176c999b202b01
Aravind Vasudevan <aravind_vasudevan@outlook.com>
2019-03-24 05:21:01 +0530

    Switched to Logger

--- src/main/java/io/github/aravindvasudev/springformdemo/BaseController.java
+++ src/main/java/io/github/aravindvasudev/springformdemo/BaseController.java
@@ -5,9 +5,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @Controller
 public class BaseController {
+    private static final Logger LOGGER = Logger.getLogger(BaseController.class.getName());
+
     @RequestMapping("/")
     public String index() {
         return "index";
@@ -15,8 +19,8 @@ public class BaseController {
 
     @RequestMapping("/submit")
     public String submit(HttpServletRequest request, Model model) {
-        System.out.printf("NAME: %s\nAGE: %s\nEMAIL: %s\n\n",
-                request.getParameter("name"), request.getParameter("age"), request.getParameter("email"));
+        LOGGER.log(Level.INFO, String.format("NAME: %s | AGE: %s | EMAIL: %s",
+                request.getParameter("name"), request.getParameter("age"), request.getParameter("email")));
 
         return "submit";
     }
